b8fc42c73261f083b31f7a8ac274fd632df43575
ogawayouji <satsumaimo697@gmail.com>
2021-07-11 19:13:43 +0900

    section1

---
+++ section1/index.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+axios.get('htttps://google.com', {
+  headers:
+});
+let hello: string = 'hello';
+console.log(hello);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
